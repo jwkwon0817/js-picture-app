f406dd13d3cdf0cfbd3128966f6204646dc310d9
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-17 21:57:30 +0900

    app JS

---
+++ app.js
@@ -0,0 +1,31 @@
+const lineWidth = document.getElementById('line-width');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+
+let isPainting = false;
+
+function onMove(event) {
+	if (isPainting) {
+		ctx.lineTo(event.offsetX, event.offsetY);
+		ctx.stroke();
+		return;
+	}
+	ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+	isPainting = true;
+}
+
+function cancelPainting() {
+	isPainting = false;
+}
+
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting)
